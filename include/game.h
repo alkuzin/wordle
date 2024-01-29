224a9cf894149701f7eeb531fb1961198e182fab
alkuzin <alkuzindev@gmail.com>
2024-01-29 22:30:18 +0300

    refactoring

--- include/game.h
+++ include/game.h
@@ -14,25 +14,32 @@
 #include <ctime>
 #include "utils.h"
 
-#define WORD_LENGTH            5 
-#define ATTEMPTS_LIMIT         5
-#define WORDLIST_LENGTH        50 
-#define WORDLIST_PATH_LENGTH   128
 #define DEFAULT_WORDLIST_PATH  "wordlist.txt"
+#define WORDLIST_PATH_LENGTH   128
+#define WORDLIST_LENGTH        50 
+#define ATTEMPTS_LIMIT         5
+#define WORD_LENGTH            5 
 
 class Game 
 {
-	char wordlist_path[WORDLIST_PATH_LENGTH];
-	char word[WORD_LENGTH + 1];
-	u32  attempts;
+	private:
+		char wordlist_path[WORDLIST_PATH_LENGTH];
+	
+	protected:
+		char hidden_word[WORD_LENGTH + 1];
+		u32  attempts;
 
 	public:
 		Game(void);
-		Game(const char *wordlist_path, u32 attempts);
+		
 		void process_input(const char *input, bool *letters);
 		void set_wordlist_path(const char *wordlist_path);
+		bool is_guessed(const bool *letters);
+		void decrement_attempts(void);
+		void update_hidden_word(void);
 		char *get_hidden_word(void);
-		void set_word(void);
+		void reset_attempts(void);
+		u32  get_attempts(void);
 };
 
 #endif // GAME_H
