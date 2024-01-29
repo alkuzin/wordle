b21ba2c8456e4acef6eec4f1d2296796cf9d4e8a
alkuzin <alkuzindev@gmail.com>
2024-01-29 22:31:49 +0300

    the UI class inherits from the Game class

--- include/ui.h
+++ include/ui.h
@@ -7,35 +7,16 @@
 #ifndef UI_H
 #define UI_H
 
-#include <iostream>
-#include <cstring>
-#include <vector>
-#include "utils.h"
+#include "game.h"
 
-#define WORD_LENGTH      5
-#define ATTEMPTS_LIMIT   5
-
-class UI 
+class UI : public Game 
 {
-	std::vector<bool *> previous_letters;
-	std::vector<char *> previous_words;
-	u32 attempts;
-
 	public:
 		UI(void);
-		~UI();
 		
-		void display(void);
-		bool is_guessed(void);
-		u32  get_attempts(void);
-		void decrement_attempts(void);
-		void save_state(const char *current_word, const bool *current_letters);
-		void display_result(void);
-	
-	private:
-		void _display_letters(void);
-		void _add_word(char *word);
-		void _add_letters(bool *letters);
+		void display_banner(void);
+		void display_result(const bool *letters);
+		void display_word(const char* word, const bool *letters);
 };
 
 #endif // UI_H
