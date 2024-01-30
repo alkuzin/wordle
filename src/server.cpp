bee93dc14897df0ef833e17e9ee62fa809f2b539
alkuzin <alkuzindev@gmail.com>
2024-01-30 14:01:29 +0300

    Updated src/server.cpp file

--- src/server.cpp
+++ src/server.cpp
@@ -10,9 +10,9 @@ Server::Server(void)
 {
 	std::memset(&server_addr, 0, sizeof(server_addr));
 	std::memset(&client_addr, 0, sizeof(client_addr));
+	
 	server_addr.sin_family      = AF_INET;
 	server_addr.sin_port        = DEFAULT_PORT;
-	//server_addr.sin_addr.s_addr = inet_addr(INADDR_ANY);
 	server_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP_ADDRESS);
 }
 
@@ -42,7 +42,8 @@ Server::~Server(void)
 {
 	close(sockfd);
 	shutdown(sockfd, SHUT_RDWR);
-	log("server", "shutdown");
+	
+	_log("server", "shutdown");
 }
 
 void Server::init(void)
@@ -50,15 +51,14 @@ void Server::init(void)
 	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
 		throw SOCKET_CREATION_EXCEPTION;	
 
-	log("server", "socket creation successfull");	
+	_log("server", "socket creation successfull");	
 
 	// Display server ip address and port
 	char ip_addr_buffer[INET_ADDRSTRLEN];
 
 	// Convert IP address to string
 	inet_ntop(AF_INET, &(server_addr.sin_addr.s_addr), ip_addr_buffer, INET_ADDRSTRLEN);
-	ip_addr_buffer[INET_ADDRSTRLEN] = '\0';
-	logf("server", "ip: %s port: %hu\n", ip_addr_buffer, server_addr.sin_port);
+	_logf("server", "ip: %s port: %hu\n", ip_addr_buffer, server_addr.sin_port);
 
 	_bind();
 
@@ -71,7 +71,7 @@ void Server::_bind(void)
 	if((bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0)
 		throw BIND_EXCEPTION;	
 
-	log("server", "bind successfull");
+	_log("server", "bind successfull");
 }
 
 void Server::_handle_client(void)
@@ -87,65 +87,65 @@ void Server::_handle_client(void)
 	// show client information
 	inet_ntop(AF_INET, &(client_addr.sin_addr.s_addr), ip_addr_buffer, INET_ADDRSTRLEN);
 	
-	logf("server", "connected client <client> [ip: %s port: %hu]\n", ip_addr_buffer,
-		 client_addr.sin_port);
-		
 	game.update_hidden_word();
 	std::memset(hidden_word, 0, sizeof(hidden_word));
 	std::strncpy(hidden_word, game.get_hidden_word(), WORD_LENGTH);
-	hidden_word[WORD_LENGTH] = '\0';
 	
-	logf("server", "hidden word: '%s'\n", hidden_word);
-
-	while(game.get_attempts()) {
+	u32 attempts;
+	_logf("server", "hidden word: '%s'\n", hidden_word);
+	char attempts_bytes[32];
+	
+	attempts = ATTEMPTS_LIMIT; 
+	while((attempts = game.get_attempts())) {
 		std::memset(message, 0, sizeof(message));
 		std::memset(letters, 0, sizeof(letters));
 		std::memset(bytes,   0, sizeof(bytes));
-	
-		log("server", "waiting client response");
+		
+		snprintf(attempts_bytes, sizeof(attempts_bytes), "%u", attempts);
+		attempts_bytes[32] = '\0'; 
+		_logf("server", "attempts left: %u\n", attempts);
+		
+		_log("server", "waiting client response");
 		recvfrom(sockfd, message, sizeof(message),
 				 MSG_WAITALL, (struct sockaddr *)&client_addr, &client_addr_len);
-		message[WORD_LENGTH] = '\0';
 		
-		logf("server", "<client>: %s\n", message);
-		log("server", "processing word");
+		_logf("server", "receiving message from client [ip: %s port: %hu]\n", ip_addr_buffer,
+			  client_addr.sin_port);
+		_logf("server", "<client>: %s\n", message);
+		
+		_log("server", "processing word");
 		game.process_input(message, letters);
 		result = game.is_guessed(letters);
 		
-		logf("server", "%s", "converting bool array to bytes: ");
-		convert_to_bytes(letters, bytes, sizeof(bytes));
-		
+		_logf("server", "%s", "converting bool array to bytes: ");
+		_convert_to_bytes(letters, bytes, sizeof(bytes));
+	
 		// display bool array of guesssed letters
-		putchar('{');
-		for(int i = 0; i < WORD_LENGTH; i++) {
-			printf(" %d", bytes[i]);
-			if(i < WORD_LENGTH - 1)
-				putchar(',');
-		}
-		putchar(' ');
-		putchar('}');
-		putchar('\n');
-		
-		log("server", "sending bytes array to client");
-		int sent_bytes = sendto(sockfd, bytes, sizeof(bytes),
+		_display_bytes(bytes, WORD_LENGTH);	
+
+		_log("server", "sending bytes array to client");
+		sendto(sockfd, bytes, sizeof(bytes),
 		MSG_CONFIRM, (struct sockaddr *)&client_addr, sizeof(client_addr));
 
-		logf("server", "sent %d bytes to client\n", sent_bytes);
-	
+		sendto(sockfd, attempts_bytes, sizeof(attempts_bytes), MSG_CONFIRM,
+			   (struct sockaddr *)&client_addr, sizeof(client_addr));
+		
 		if(result) {
-			log("server", "client finished game");
+			_log("server", "client finished game");
 			std::cout << std::endl;
-			log("server", "setting new hidden word");
+			
+			_log("server", "setting new hidden word");
 			
 			game.update_hidden_word();
 			std::memset(hidden_word, 0, sizeof(hidden_word));
 			std::strncpy(hidden_word, game.get_hidden_word(), WORD_LENGTH);
-			hidden_word[WORD_LENGTH] = '\0';
-			logf("server", "hidden word: '%s'\n", hidden_word);
+
+			_logf("server", "hidden word: '%s'\n", hidden_word);
+
+			_log("server", "reset attempts");
+			game.set_attempts(ATTEMPTS_LIMIT);
 			continue;
 		}
 		game.decrement_attempts();
 	}
-	log("server", "reset attempts");
-	game.reset_attempts();
 }
