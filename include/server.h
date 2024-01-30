1e0d9ecdc55492034685de98d3bebd0ccf57dfcb
alkuzin <alkuzindev@gmail.com>
2024-01-30 14:00:22 +0300

    refactoring

--- include/server.h
+++ include/server.h
@@ -7,33 +7,33 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <iostream>
-#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <iostream>
+#include <cstring>
 #include "client.h"
 #include "utils.h"
 #include "game.h"
 
-#define DEFAULT_IP_ADDRESS  "127.0.0.123"
+#define DEFAULT_IP_ADDRESS  "127.0.0.1"
 #define DEFAULT_PORT       	12345 
-#define WORD_LENGTH         5
-#define EXIT_CODE 			"-----"
+#define WORD_LENGHT         5
 
 class Server 
 {
 	struct sockaddr_in server_addr;
 	struct sockaddr_in client_addr;
-	int sockfd;	
+	int  sockfd;	
 	Game game;
 
 	public:
 		Server();
 		Server(const char *ip_addr, u16 port, const char *wordlist_path);
 		~Server();
+		
 		void init(void);
 	
 	private:
