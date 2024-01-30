56a56672d7c27987b81cb2c240cc193994279315
alkuzin <alkuzindev@gmail.com>
2024-01-30 14:01:29 +0300

    Updated src/client.cpp file

--- src/client.cpp
+++ src/client.cpp
@@ -12,6 +12,16 @@ Client::Client(void)
 	server_addr.sin_family      = AF_INET;
 	server_addr.sin_port        = DEFAULT_PORT;
 	server_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP_ADDRESS);
+	
+	_set_client_name();
+}
+
+void Client::_set_client_name(void)
+{
+	std::memset(client_name, 0, sizeof(client_name));
+	
+	if (getlogin_r(client_name, sizeof(client_name)) != 0)
+    	std::strncpy(client_name, DEFAULT_CLIENT_NAME, sizeof(client_name));
 }
 
 Client::Client(const char *ip_addr, u16 port)
@@ -30,6 +40,8 @@ Client::Client(const char *ip_addr, u16 port)
 	server_addr.sin_family      = AF_INET;
 	server_addr.sin_port        = port;
 	server_addr.sin_addr.s_addr = inet_addr(ip_addr);
+	
+	_set_client_name();
 }
 
 Client::~Client(void) {
@@ -49,55 +61,48 @@ void Client::_handle_server(void)
 	char message[WORD_LENGTH + 1];
 	bool letters[WORD_LENGTH];
 	u8   bytes[WORD_LENGTH + 1];
+	char attempts_bytes[32];
 	u32  server_addr_len;
 		
 	system("clear");
 	ui.display_banner();
-
-	while(ui.get_attempts()) {
+	
+	do {
+		std::memset(attempts_bytes, 0, sizeof(attempts_bytes));
 		std::memset(letters, 0, sizeof(letters));
 		std::memset(message, 0, sizeof(message));
 		std::memset(bytes,   0, sizeof(bytes));
 
-		printf("<client>: ");
-		getinput(message, sizeof(message));
-		message[WORD_LENGTH] = '\0';
+		printf("<%s>: ", client_name);
+		_getinput(message, sizeof(message));
 
 		if(message[0] == '\0')
 			continue;
 	
-		//logf("client", "sending word '%s'\n", message);
 		// sending word
 		sendto(sockfd, message, sizeof(message), MSG_CONFIRM,
 			  (struct sockaddr *)&server_addr, sizeof(server_addr));
-		
-		//logf("client", "sent %d bytes \n", sent_bytes);
 
 		// receiving bytes array
-		//log("client", "receiving bytes array");
 		recvfrom(sockfd, bytes, sizeof(bytes), MSG_WAITALL,
 		        (struct sockaddr *)&server_addr, &server_addr_len);
-		bytes[WORD_LENGTH] = '\0';
 			
-		//logf("client", "received %d bytes \n", recv_bytes);
-		//log("client", "received bytes array");
-		/*
-		for(int i = 0; i < WORD_LENGTH; i++)
-             printf(" %02x", bytes[i]);
-        putchar('\n');
-		*/
-
 		// converting bytes array to bool array
-		convert_to_bool(bytes, letters, sizeof(letters));
+		_convert_to_bool(bytes, letters, sizeof(letters));
+		
+		//receiving number of attempts left
+		recvfrom(sockfd, attempts_bytes, sizeof(attempts_bytes), MSG_WAITALL,
+		        (struct sockaddr *)&server_addr, &server_addr_len);
+		ui.set_attempts(std::atoi(attempts_bytes));
 		
 		ui.display_word(message, letters);
 		
 		if(ui.is_guessed(letters))
 			break;
-
+		
 		ui.decrement_attempts();
-	}
-	//system("clear");
-	//ui.display();
+
+	} while(ui.get_attempts());
+	
 	ui.display_result(letters);
 }
