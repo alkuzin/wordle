dd3ec7deb0b53ec939d559eb65c6479fd3a2e3e5
alkuzin <alkuzindev@gmail.com>
2024-02-01 15:23:41 +0300

    added shared memory for hidden word

--- include/server.h
+++ include/server.h
@@ -18,6 +18,12 @@
 #include "utils.h"
 #include "game.h"
 
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define SHARED_MEMORY_OBJECT_NAME "server_shared_memory"
+#define SHARED_MEMORY_OBJECT_SIZE 128
+
 #define DEFAULT_IP_ADDRESS   "127.0.0.1"
 #define DEFAULT_PORT       	 12345 
 #define WORD_LENGHT          5
@@ -28,8 +34,11 @@ class Server
 {
 	struct sockaddr_in server_addr;
 	struct sockaddr_in client_addr;
-	int  sockfd;	
+	int  sockfd;
 	Game game;
+	
+	int _shm;
+	char *_addr;
 
 	public:
 		Server();
@@ -37,6 +46,7 @@ class Server
 		~Server();
 		
 		void init(void);
+		void init_shared_memory(int shm, char *addr);
 	
 	private:
 		void _bind(void);
@@ -44,6 +54,11 @@ class Server
 		void _show_server_info(void);
 		void _show_client_info(void);
 		void _get_client_ip(char *buffer);
+
+		void _open_shared_memory(void);
+		void _create_shared_memory(void);
+		void _fill_shared_memory(char *data, size_t size);
+		void _close_shared_memory(void);
 };
 
 #endif // SERVER_H
