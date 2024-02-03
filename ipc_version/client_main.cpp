45137e4fa58a7cac77ac1dc8df7e247e53fbd698
alkuzin <alkuzindev@gmail.com>
2024-02-03 20:09:46 +0300

    Updated ./client_main.cpp file

--- ipc_version/client_main.cpp
+++ ipc_version/client_main.cpp
@@ -9,21 +9,46 @@
 int main(void) 
 {
 	try {
-		key_t key = ftok(SHARED_MEMORY_BLOCK_NAME, SHARED_MEMORY_BLOCK_SIZE);
+		key_t key; 
+    	int shmid;
+		char *block;
+		
+		sem_t *sem_server;
+		sem_t *sem_client;
+		
+		key = ftok(SHARED_MEMORY_BLOCK_NAME, SHARED_MEMORY_BLOCK_SIZE);
  
     	// shmget returns an identifier in shmid
-    	int shmid = shmget(key, SHARED_MEMORY_BLOCK_SIZE, 0666 | IPC_CREAT);
+    	shmid = shmget(key, SHARED_MEMORY_BLOCK_SIZE, 0666 | IPC_CREAT);
  
     	// shmat to attach to shared memory
-    	char* block = (char *)shmat(shmid, NULL, 0);
+    	block = (char *)shmat(shmid, NULL, 0);
 		_logf("server", "set shared memory block: <%p>\n", block);
- 
-		Client client(block);
+
+		// setup semaphores
+		//sem_unlink(SEM_SERVER_NAME);
+		//sem_unlink(SEM_CLIENT_NAME);
+		
+		sem_server = sem_open(SEM_SERVER_NAME, O_CREAT, 0777, 0);
+		if(sem_server == SEM_FAILED) {
+			perror("sem_open/sem_server");
+			exit(EXIT_FAILURE);
+		}
+		_log("client", "sem_server opened");
+		
+		sem_client = sem_open(SEM_CLIENT_NAME, O_CREAT, 0777, 0);
+		if(sem_client == SEM_FAILED) {
+			perror("sem_open/sem_client");
+			exit(EXIT_FAILURE);
+		}
+		_log("client", "sem_client opened");
+		
+
+		Client client(block, sem_server, sem_client);
 		client.init();
  
     	// detach from shared memory
     	shmdt(block);
-
 	}
 	catch(exception e) 
 	{
