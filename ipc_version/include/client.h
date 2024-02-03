189cfd49f1f682f7be96abc98aabca772990144f
alkuzin <alkuzindev@gmail.com>
2024-02-03 17:18:06 +0300

    Updated ./include file

--- ipc_version/include/client.h
+++ ipc_version/include/client.h
@@ -7,34 +7,25 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <iostream>
 #include <cstring>
+#include <unistd.h>
+
 #include "utils.h"
 #include "ui.h"
 
-#define DEFAULT_IP_ADDRESS   "127.0.0.1"
-#define DEFAULT_PORT         12345
 #define DEFAULT_CLIENT_NAME  "client"
 #define CLIENT_NAME_SIZE     256
 #define ATTEMPTS_BYTES_SIZE  12
-#define CLIENT_INVITATION    "#####"
 
 class Client 
 {
-	struct sockaddr_in server_addr;
 	char   client_name[CLIENT_NAME_SIZE];
-	int    sockfd;	
 	UI     ui;
+	char *shm_addr;
 
 	public:
-		Client();
-		Client(const char *ip_addr, u16 port);
-		~Client();
+		Client(char *addr);
 		
 		void init(void);
 	
--- ipc_version/include/server.h
+++ ipc_version/include/server.h
@@ -7,58 +7,25 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <iostream>
 #include <cstring>
-#include "client.h"
+#include <unistd.h>
 #include "utils.h"
 #include "game.h"
 
-#include <sys/mman.h>
-#include <fcntl.h>
-
-#define SHARED_MEMORY_OBJECT_NAME "server_shared_memory"
-#define SHARED_MEMORY_OBJECT_SIZE 128
-
-#define DEFAULT_IP_ADDRESS   "127.0.0.1"
-#define DEFAULT_PORT       	 12345 
-#define WORD_LENGHT          5
-#define ATTEMPTS_BYTES_SIZE  12
-#define CLIENT_INVITATION    "#####"
-
 class Server 
 {
-	struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
-	int  sockfd;
+	char *shm_addr;
 	Game game;
-	
-	int _shm;
-	char *_addr;
 
 	public:
-		Server();
-		Server(const char *ip_addr, u16 port, const char *wordlist_path);
-		~Server();
+		Server(char *addr);
 		
 		void init(void);
-		void init_shared_memory(int shm, char *addr);
-	
-	private:
-		void _bind(void);
-		void _handle_client(void);
-		void _show_server_info(void);
-		void _show_client_info(void);
-		void _get_client_ip(char *buffer);
+		void send(char *message);
 
-		void _open_shared_memory(void);
-		void _create_shared_memory(void);
-		void _fill_shared_memory(char *data, size_t size);
-		void _close_shared_memory(void);
+	private:
+		void _handle_client(void);
 };
 
 #endif // SERVER_H
