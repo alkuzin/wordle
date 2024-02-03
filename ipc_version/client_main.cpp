1069c2ccd9acb22beb61524062d132370dd85c43
alkuzin <alkuzindev@gmail.com>
2024-02-03 17:18:06 +0300

    Updated ./client_main.cpp file

--- ipc_version/client_main.cpp
+++ ipc_version/client_main.cpp
@@ -6,47 +6,30 @@
 
 #include "include/client.h"
 
-int main(int argc, char **argv) 
+int main(void) 
 {
 	try {
+		key_t key = ftok(SHARED_MEMORY_BLOCK_NAME, SHARED_MEMORY_BLOCK_SIZE);
+ 
+    	// shmget returns an identifier in shmid
+    	int shmid = shmget(key, SHARED_MEMORY_BLOCK_SIZE, 0666 | IPC_CREAT);
+ 
+    	// shmat to attach to shared memory
+    	char* block = (char *)shmat(shmid, NULL, 0);
+		_logf("server", "set shared memory block: <%p>\n", block);
+ 
+		Client client(block);
+		client.init();
+ 
+    	// detach from shared memory
+    	shmdt(block);
 
-		if(argc > 3) {
-			std::cerr << "client: too many arguments" << std::endl;
-			exit(EXIT_FAILURE);
-		}
-
-		if(argc == 3) {
-			char ip_addr[INET_ADDRSTRLEN];
-			u16  port;	
-
-			std::memset(ip_addr, 0, sizeof(ip_addr));
-			std::strncpy(ip_addr, argv[1], INET_ADDRSTRLEN);
-			port = atoi(argv[2]);
-
-			Client client(ip_addr, port);
-			client.init();
-		}
-		else {
-			Client client;
-			client.init();
-		}
 	}
 	catch(exception e) 
 	{
 		std::cerr << "exception: ";
+		
 		switch(e) {
-			case INVALID_IP_ADDRESS_EXCEPTION:
-				std::cerr << "this ip address is invalid" << std::endl;
-				break;
-
-			case FORBIDDEN_PORT_EXCEPTION:
-				std::cerr << "this port is not allowed" << std::endl;
-				break;
-
-			case SOCKET_CREATION_EXCEPTION:
-				std::cerr << "socket creation failed" << std::endl;
-				break;
-			
 			case MEMORY_ALLOCATION_EXCEPTION:
 				std::cerr << "memory allocation error" << std::endl;
 				break;
