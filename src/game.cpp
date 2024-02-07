b4a469d640f44637c4c2f9752482cfe7b9ab882a
alkuzin <alkuzindev@gmail.com>
2024-02-07 17:15:22 +0300

    feat: added input processing for IPC version

---
+++ src/game.cpp
@@ -0,0 +1,126 @@
+/*
+ * game.cpp
+ * @alkuzin - 2024
+ *
+ */
+
+#include "../include/game.h"
+
+Game::Game(void) 
+{
+	std::strncpy(wordlist_path, DEFAULT_WORDLIST_PATH, WORDLIST_PATH_LENGTH);
+	attempts = ATTEMPTS_LIMIT;
+	set_attempts(ATTEMPTS_LIMIT);
+
+	_set_hidden_words();
+}
+
+void Game::process_input(char *input, bool *letters, char *result, u32 size)
+{
+	std::string res;
+	u32 counter;
+	u32 i;
+
+	i = 0;
+	counter = 0;
+	
+	res += "\"";
+	res += input;
+	res += "\" - ";
+	while(input[i]) {
+		letters[i] = hidden_word[i] == input[i];
+		if(letters[i]) {
+			res += " " + std::to_string(i + 1);
+			
+			if(i < WORD_LENGTH - 1)
+				res += ",";
+			
+			counter++;
+		}
+		i++;
+	}
+
+	if(!counter)
+		res += "there isn't any correct letters";
+	else
+		res += " letters are correct";
+
+	std::strncat(result, res.c_str(), size);
+	result[res.size()] = '\0';
+}
+
+void Game::process_input(const char *input, bool *letters)
+{
+	u32 i;
+
+	i = 0;
+	while(input[i]) {
+		letters[i] = (hidden_word[i] == input[i]);	
+		i++;
+	}
+	
+	while(i < WORD_LENGTH) {
+		letters[i] = false;	
+		i++;
+	}
+}
+
+void Game::set_wordlist_path(const char *wordlist_path)
+{
+	std::memset(this->wordlist_path, 0, sizeof(wordlist_path));
+	std::strncpy(this->wordlist_path, wordlist_path, WORDLIST_PATH_LENGTH);
+}
+
+void Game::update_hidden_word(void) 
+{
+	u32 random_word_index; 	
+
+	srand(time(NULL));
+	random_word_index = rand() % (WORDLIST_LENGTH - 1) + 1;
+
+	std::strncpy(hidden_word, hidden_words.at(random_word_index).c_str(), WORD_LENGTH);
+	hidden_word[WORD_LENGTH] = '\0';
+}
+
+void Game::_set_hidden_words(void)
+{
+	std::ifstream wordlist_file;
+	std::string word;
+
+	wordlist_file.open(wordlist_path, std::ios::in);
+	
+	if(!wordlist_file.is_open())
+		throw WORDLIST_FILE_OPEN_EXCEPTION;
+
+	while(std::getline(wordlist_file, word))
+		hidden_words.push_back(word);	
+	
+	wordlist_file.close();	
+}
+
+char *Game::get_hidden_word(void) {
+	return hidden_word;
+}
+
+bool Game::is_guessed(const bool *letters)
+{
+	for(int i = 0; i < WORD_LENGTH; i++) {
+		if(!letters[i])
+			return false;
+	}
+	
+	return true;
+}
+
+void Game::set_attempts(u32 attempts) {
+	this->attempts = attempts;
+}
+
+u32 Game::get_attempts(void) {
+	return attempts;
+}
+
+void Game::decrement_attempts(void) {
+	if(attempts)
+		attempts--;
+}
