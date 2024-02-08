d14beac422e09e38810eb6b7264a18feeea540af
alkuzin <alkuzindev@gmail.com>
2024-02-08 23:21:46 +0300

    refactor: removed extra comments

--- src/ipc_server.cpp
+++ src/ipc_server.cpp
@@ -26,9 +26,8 @@ void IPC_Server::begin(void)
 	_log("server", "sem_wait(sem_server)");
 	sem_wait(sem_server);
 	
-	if(!shm_addr[0]) {
+	if(!shm_addr[0])
 		sem_wait(sem_server);
-	}
 }
 
 void IPC_Server::end(void)
@@ -67,102 +66,3 @@ void IPC_Server::_shutdown(void) {
 IPC_Server::~IPC_Server(void) {
 	_shutdown();
 }
-
-
-/*void IPC_Server::_handle_client(void)
-{
-	char result[SHARED_MEMORY_BLOCK_SIZE];
-	char hidden_word[WORD_LENGTH + 1];
-	bool letters[WORD_LENGTH];
-	char attempts_buffer[UTOA_SIZE];
-	char buffer[WORD_LENGTH + 1];
-	u32  attempts;
-
-	// set hidden word
-	game.update_hidden_word();
-	std::memset(hidden_word, 0, sizeof(hidden_word));
-	std::strncpy(hidden_word, game.get_hidden_word(), WORD_LENGTH);
-	_logf("server", "set hidden word: (%s)\n", hidden_word);	
-
-	attempts = ATTEMPTS_LIMIT;
-	_logf("server", "set attempts limit: (%u)\n", attempts);	
-	
-	do {
-		std::memset(attempts_buffer, 0, sizeof(attempts_buffer));
-		std::memset(shm_addr, 0, SHARED_MEMORY_BLOCK_SIZE);
-		std::memset(result, 0, sizeof(result));
-
-		attempts = game.get_attempts();
-		
-		// waiting for client
-		_log("server", "waiting for client");
-		_log("server", "sem_wait(sem_server)");
-		sem_wait(sem_server);
-		
-		if(!shm_addr[0]) {
-			_log("server", "sem_wait(sem_server)");
-			sem_wait(sem_server);
-		}
-
-		// receiving & processing word 
-		recv(buffer, WORD_LENGTH + 1);
-		std::cout << std::endl;
-		_logf("server", "client send message: (%s)\n", buffer);
-		
-		game.process_input(buffer, letters, result, SHARED_MEMORY_BLOCK_SIZE);
-		
-		if(game.is_guessed(letters)) {
-			_log("server", "client win the game");	
-			send((char *)"WIN", 4);
-			break;
-		}
-		
-		if((attempts == 1) && !game.is_guessed(letters)) {
-			_log("server", "client lose the game");	
-			send((char *)"LOSE", 5);
-			break;
-		}
-
-		_logf("server", "processing result: (%s)\n", result);	
-		send(result, SHARED_MEMORY_BLOCK_SIZE);
-		
-		// liberate client
-		_log("server", "sem_post(sem_client)");
-		sem_post(sem_client);
-		
-		// waiting for client
-		_log("server", "sem_wait(sem_server)");
-		sem_wait(sem_server);
-	
-		// sending the number of attempts left
-		snprintf(attempts_buffer, UTOA_SIZE, "%u", attempts);
-		send(attempts_buffer, UTOA_SIZE);
-
-		game.decrement_attempts();
-		_logf("server", "(attempts left: %u)\n", game.get_attempts());
-		
-		// liberate client
-		_log("server", "sem_post(sem_client)");
-		sem_post(sem_client);
-		
-		// waiting for client
-		_log("server", "sem_wait(sem_server)");
-		sem_wait(sem_server);
-
-	} while(game.get_attempts());
-	
-	// liberate client
-	_log("server", "sem_post(sem_client)");
-	sem_post(sem_client);
-		
-	// waiting for client
-	_log("server", "sem_wait(sem_server)");
-	sem_wait(sem_server);
-			
-	//liberate server
-	_log("server", "sem_post(sem_server)");
-	sem_post(sem_server);
-	
-	game.set_attempts(ATTEMPTS_LIMIT);
-	_log("server", "--- client finished game ---");
-}*/
