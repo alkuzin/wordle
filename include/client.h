36fa953812f16a09992f928dff33e94905caf330
alkuzin <alkuzindev@gmail.com>
2024-02-08 23:12:00 +0300

    feat: added begin and end methods

--- include/client.h
+++ include/client.h
@@ -16,6 +16,8 @@ class Client
 		void virtual init(void) = 0;
 		void virtual recv(char *message, size_t size) = 0;
 		void virtual send(char *message, size_t size) = 0;
+		void virtual begin(void) = 0;
+		void virtual end(void) = 0;
 	
 	protected:
 		void virtual _shutdown(void) = 0;
